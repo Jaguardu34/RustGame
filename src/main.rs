677f05f7f1c71ac33155003686483eef447b77a2
Jaguardu34 <lucas.reute@outlook.fr>
2026-07-21 23:10:52 +0200

    basic bevy tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,62 @@
 use bevy::prelude::*;
 
 
-fn hello_world() {
-    println!("hello world!");
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (update_people, greet_people).chain());
+    }
 }
 
-fn main() {
-    App::new().add_systems(Update, hello_world).run();
+
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Jean".to_string())));
+    commands.spawn((Person, Name("Lucas".to_string())));
+    commands.spawn((Person, Name("Mathis".to_string())));
+    
+    
 }
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>){
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {} !", name.0);
+        }
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Lucas" {
+            name.0 = "Lucas le plus beau".to_string();
+            break; // We don't need to change any other names.
+        }
+    }
+}
+
+
+
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
+        .run();
+}
+
+
+
